474330b40922813aedc28a129dad964e1af5bfd7
Spencer197 <spncr@protonmail.com>
2018-02-21 14:57:57 +0000

    Add devise and bootstrap for auth and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception#This line must come before the before_action line.
+  before_action :authenticate_user!#Ensures that a user is logged in before actions can be taken in the app.
 end
