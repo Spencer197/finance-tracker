352913b106a8eca50f2767180dae7120a42152dd
Spencer197 <spncr@protonmail.com>
2018-03-04 13:42:22 +0000

    Add stock listing & restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def my_portfolio
-    
+    @user = current_user#Ensures user is logged in.
+    @user_stocks = current_user.stocks#Returns all of the user's stocks.
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
          
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?#Ensures that the user's id & the stock id are both in the database.
+  end
+  
+  def under_stock_limit?#Sets a limit of ten stocks in one's portfolio.  
+    (user_stocks.count < 10)
+  end
+  
+  def can_add_stock?(ticker_symbol)#Ensures that the allowable stock count has not been exceeded & that the stock doesn't already exist.
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
 end
